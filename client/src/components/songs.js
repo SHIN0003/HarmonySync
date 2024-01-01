7926ca0af36f1e5ec143ba2f8851f0d82ed41e78
Andrew Tai <andrewtai@umass.edu>
2024-01-01 18:07:16 -0500

    updated songs unique id

--- client/src/components/songs.js
+++ client/src/components/songs.js
@@ -9,43 +9,42 @@ const Songs = () => {
     const [tracks, setTracks] = useState([]);
     const accessToken = localStorage.getItem('accessToken');
 
-    useEffect(() => {
-        const fetchAllTracks = async (url) => {
-          const allTracks = [];
-          let nextUrl = url;
-          
-          while (nextUrl) {
-            const response = await axios.get(nextUrl, {
-              headers: { 'Authorization': `Bearer ${accessToken}` }
-            });
-            allTracks.push(...response.data.items);
-            nextUrl = response.data.next; // Spotify provides the next set of items in 'next'
-          }
-          
-          return allTracks;
-        };
-    
-        const fetchPlaylistDetails = async () => {
-          try {
-            const detailsUrl = `https://api.spotify.com/v1/playlists/${playlistId}`;
-            const detailsResponse = await axios.get(detailsUrl, {
-              headers: { 'Authorization': `Bearer ${accessToken}` }
-            });
-            setPlaylistDetails(detailsResponse.data);
-    
-            const tracksUrl = `${detailsUrl}/tracks?limit=100`;
-            const allTracks = await fetchAllTracks(tracksUrl);
-            setTracks(allTracks);
-          } catch (error) {
-            console.error('Error fetching playlist details:', error);
-          }
-        };
-    
-        if (accessToken) {
-          fetchPlaylistDetails();
-        }
-      }, [playlistId, accessToken]);
-    
+  useEffect(() => {
+    const fetchAllTracks = async (url) => {
+      const allTracks = [];
+      let nextUrl = url;
+      
+      while (nextUrl) {
+        const response = await axios.get(nextUrl, {
+          headers: { 'Authorization': `Bearer ${accessToken}` }
+        });
+        allTracks.push(...response.data.items);
+        nextUrl = response.data.next; // Spotify provides the next set of items in 'next'
+    }
+      return allTracks;
+    };
+
+    const fetchPlaylistDetails = async () => {
+      try {
+        const detailsUrl = `https://api.spotify.com/v1/playlists/${playlistId}`;
+        const detailsResponse = await axios.get(detailsUrl, {
+          headers: { 'Authorization': `Bearer ${accessToken}` }
+        });
+        setPlaylistDetails(detailsResponse.data);
+
+        const tracksUrl = `${detailsUrl}/tracks?limit=100`;
+        const allTracks = await fetchAllTracks(tracksUrl);
+        setTracks(allTracks);
+      } catch (error) {
+        console.error('Error fetching playlist details:', error);
+      }
+    };
+
+    if (accessToken) {
+      fetchPlaylistDetails();
+    }
+  }, [playlistId, accessToken]);
+
   return (
     <Container>
       {playlistDetails && (
@@ -62,7 +61,7 @@ const Songs = () => {
           <Row className="mt-4">
             <ListGroup>
               {tracks.map((track, index) => (
-                <ListGroup.Item key={track.track.id}>
+                <ListGroup.Item key={track.track.id + index}>
                   {index + 1}. {track.track.name} by {track.track.artists.map(artist => artist.name).join(', ')}
                 </ListGroup.Item>
               ))}
--- client/src/index.js
+++ client/src/index.js
@@ -7,11 +7,11 @@ import { AuthProvider} from './contexts/authContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
