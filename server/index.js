6a4b4612d81db1b3220eeceed547f8c6312cba95
Andrew Tai <andrewtai@umass.edu>
2024-01-05 18:22:35 -0500

    try 2 with correct callback

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const path = require('path');
 const app = express();
 app.use(cors({
-  origin: 'https://harmonysyncserver.onrender.com', // adjust if your frontend port is different
+  origin: 'https://harmonysyncserver.onrender.com/3001', // adjust if your frontend port is different
   credentials: true
 }));
 const session = require('express-session');
